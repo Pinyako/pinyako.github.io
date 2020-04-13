b296424d22f6ddc2efddcf2abaee54b5baec6a68
Pinyako <773091546@qq.com>
2020-04-13 19:17:59 +0800

    Site updated: 2020-04-13 19:17:58

--- Leetcode/temp.py
+++ Leetcode/temp.py
@@ -1,18 +1,15 @@
-def copy_string(text):
-    a = ''
-    s = len(text)
-    for i in text:
-        if i == 'o':
-            print('return here')
-            return
-        a += i
-    else:
-        print(a[s-1])
+import os
 
-if __name__ == '__main__':
-    text = 'Helloworld'
-    try:    
-        copy_string(text)
-    except Exception as e:
-        print(e)
-        
+hidden: true
+
+posts = [i for i in os.listdir() if str(i).find('.md')]
+
+
+for p in posts:
+    print(p)
+    lines = open(p, 'r', encoding='UTF-8').readlines()
+    lines.insert(2, 'hidden: true\n')
+    for i, l in enumerate(lines):
+        if l == '  - hide\n':
+            lines.pop(i)
+    open(p, 'w', encoding='UTF-8').writelines(lines)
